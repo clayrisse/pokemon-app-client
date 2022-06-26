06983f61f0c77e3d3931c60d6973a9b55e74ea6a
Rocio <roci.ramos87@gmail.com>
2022-06-26 16:11:31 +0200

    last commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,12 +6,21 @@ import { Component, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  option: number;
   constructor() { 
-   
+   this.option = 0;
   }
 
   ngOnInit(): void {
+  }
+  changeComponent1(){
+    this.option = 1;
+  }
+  changeComponent2(){
+    this.option = 2;
+  }
+  changeComponent3(){
+    this.option = 3;
   }
 
 }
